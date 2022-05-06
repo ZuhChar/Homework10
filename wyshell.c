8bd23458da01d4b777459c7326b1154f1cad194e
ZuhChar <zcrimmel@uwyo.edu>
2022-05-05 22:47:52 -0600

    l

--- wyshell.c
+++ wyshell.c
@@ -161,10 +161,6 @@ int main()
                     wait(NULL);
                 }
                 prevUse = 1;
-                for (int i = 1; i < 100; i++)
-                {
-                    arguments[i] = NULL;
-                }
                 break;
             case SEMICOLON:
                 if (fork() == 0)
@@ -206,6 +202,10 @@ int main()
             if (eol == 1)
             {
                 break;
+            }
+            for (int i = 1; i < 100; i++)
+            {
+                arguments[i] = NULL;
             }
         }
         // Create a child to run the command in
